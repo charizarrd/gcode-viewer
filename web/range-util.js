72ae1a52ed53fc0d29ee8c7bcc1c022f57c61c7d
Weston C. Beecroft <weston@voxel8.co>
2016-07-21 15:41:23 -0400

    Implement 'intersectRanges'

--- web/range-util.js
+++ web/range-util.js
@@ -7,3 +7,27 @@ RangeUtil.unionRanges = function(range1Start, range1End, range2Start, range2End)
 
     return range;
 };
+
+RangeUtil.intersectRanges = function(range1Start, range1End, range2Start, range2End) {
+    var range = [];
+
+    var limit1 = 0;
+    var limit2 = 0;
+
+    if (range1Start >= range2Start && range1Start <= range2End) {
+        limit1 = range1Start;
+    } else if (range2Start >= range1Start && range2Start <= range1End) {
+        limit1 = range2Start;
+    }
+
+    if (range1End <= range2End && range1End >= range2Start) {
+        limit2 = range1End;
+    } if (range2End <= range1End && range2End >= range1Start) {
+        limit2 = range2End;
+    }
+
+    range[0] = limit1;
+    range[1] = limit2;
+
+    return range;
+};
