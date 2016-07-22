1cacbe978518398c88557b1fde96819dc1081a18
Weston C. Beecroft <weston@voxel8.co>
2016-07-22 13:51:56 -0400

    Document/clarify 'intersectRangeSets'

--- web/range-util.js
+++ web/range-util.js
@@ -26,33 +26,42 @@ RangeUtil.intersectRangeSets = function(set1, set2) {
 
     var set1Index = 0;
     var set2Index = 0;
-    var inSet1 = false;
-    var inSet2 = false;
-    var inBothSets = false;
+
+    //As we iterate both sets, these track whether we have hit the start
+    //of a range from one set or the other, or both, without hitting the end yet
+    var inSet1Range = false;
+    var inSet2Range = false;
+    var inSet1AndSet2Ranges = false;
 
     var intersectedRangeSet = [];
 
+    //Traverse all the points from both sets in increasing order,
+    //e.g. with set1 = [3,3,5] and set2 = [2,4,8], the traversal order
+    //is 2,3,3,4,5,8.
     while (set1Index < set1.length && set2Index < set2.length) {
         var set1Element = set1[set1Index];
         var set2Element = set2[set2Index];
-        var element;
+        var element; //will hold either set1Element or set2Element
         
         if (set1Element <= set2Element) {
             element = set1Element;
-            inSet1 = set1Index % 2 === 0;
+
+            //If the index is even, it's a range start value; 
+            //otherwise it's a range end
+            inSet1Range = set1Index % 2 === 0;
             set1Index++;
         } else {
             element = set2Element;
-            inSet2 = set2Index % 2 === 0;
+            inSet2Range = set2Index % 2 === 0;
             set2Index++;
         }
 
-        if (inSet1 && inSet2 && !inBothSets) {
+        if (inSet1Range && inSet2Range && !inSet1AndSet2Ranges) {
             intersectedRangeSet.push(element);
-            inBothSets = true;
-        } else if ((!inSet1 || !inSet2) && inBothSets) {
+            inSet1AndSet2Ranges = true;
+        } else if ((!inSet1Range || !inSet2Range) && inSet1AndSet2Ranges) {
             intersectedRangeSet.push(element);
-            inBothSets = false;
+            inSet1AndSet2Ranges = false;
         }
     }
 
